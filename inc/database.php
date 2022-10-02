473dc6b067d31804757760a2b8553401ebc1493c
mariobraga82 <72447751+mariobraga82@users.noreply.github.com>
2022-10-02 17:18:13 +0100

    update rds instance name

--- inc/database.php
+++ inc/database.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "mymovienet-production.cdv5owlna2ty.eu-west-3.rds.amazonaws.com";
+$servername = "showmestuff-1.ca4clfzcsn8x.eu-west-2.rds.amazonaws.com";
 $username = "mysqladmin";
 $password = "P4ssword12345";
 $dbname = "entertainment";
